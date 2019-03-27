27ec875bc08a3961e1d4526eadb042e6d00df2a2
Bram Kreuger <bramkreuger@gmail.com>
2019-03-27 14:44:56 +0100

    verwijderd nutteloze html

--- databaseInteraction.js
+++ databaseInteraction.js
@@ -1,8 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
- 
-getBooks(0, 5, "", "");
 
-function getBooks(priceMin, priceMax, author, genre)
+// --------------------- Dit is allemaal leuk en aardig ---------------------------------------
+/*var title = document.getElementById("titleFilter").value;
+var priceFrom = document.getElementsByClassName("priceFrom")[0].value;
+var priceTo = document.getElementsByClassName("priceTo")[0].value;
+document.getElementById("genreFilter");
+document.getElementById("authorFilter");
+document.getElementsByTagName("submit").onclick(getBooks(priceFrom, priceTo, title, "", ""));*/
+// ------------ Maar Node.js is serverside dus alle Node.js functies kunnen  ----------------------
+// ------------ niet in de browser worden aangeroepen -----------------------------------------
+
+// Vul hierin je gewenste atributes in en run in de terminal: node databaseInteraction.js
+getBooks(0, 25, "", "", "Fantasy");
+function getBooks(priceMin, priceMax, title, author, genre)
 {
     // open database in memory
     let db = new sqlite3.Database('bookstore.db', (err) => {
@@ -14,13 +24,16 @@ function getBooks(priceMin, priceMax, author, genre)
 
     where = " price >= " + priceMin + " AND price <= " + priceMax;
 
+    if(title != "")
+        where += " AND title = \"" + title + "\"";
     if(author != "")
         where += " AND author = \"" + author + "\"";
     if(genre != "")
         where += " AND genre = \"" + genre + "\"";
     
     //where = " price >= " + priceMin + " AND price <= " + priceMax + " AND author = \"" + author + "\" AND genre = \"" + genre + "\"";
-    
+   // console.log(`SELECT * FROM book WHERE` + where);
+
     db.serialize(() => {
         db.each(`SELECT * FROM book WHERE` + where, (err, row) => {
         if (err) {
