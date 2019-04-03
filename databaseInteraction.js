bb19e9dfd517f54561faad582fc9a614eb934ddd
Bram Kreuger <bramkreuger@gmail.com>
2019-04-03 16:54:02 +0200

    nieuwe files

--- databaseInteraction.js
+++ databaseInteraction.js
@@ -1,17 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// --------------------- Dit is allemaal leuk en aardig ---------------------------------------
-/*var title = document.getElementById("titleFilter").value;
-var priceFrom = document.getElementsByClassName("priceFrom")[0].value;
-var priceTo = document.getElementsByClassName("priceTo")[0].value;
-document.getElementById("genreFilter");
-document.getElementById("authorFilter");
-document.getElementsByTagName("submit").onclick(getBooks(priceFrom, priceTo, title, "", ""));*/
-// ------------ Maar Node.js is serverside dus alle Node.js functies kunnen  ----------------------
-// ------------ niet in de browser worden aangeroepen -----------------------------------------
-
 // Vul hierin je gewenste atributes in en run in de terminal: node databaseInteraction.js
-getBooks(0, 25, "", "", "Fantasy");
+// PriceMin en PriceMax zijn verplicht, title, autor en genre zijn optioneel
+// Wanneer je iets open wil laten geef dan "" mee
+// Bij genre kun je een string geven of een array met strings
+// VOORBEELD: getBooks(0, 25, "animal", "george", "");
 function getBooks(priceMin, priceMax, title, author, genre)
 {
     // open database in memory
@@ -25,14 +18,27 @@ function getBooks(priceMin, priceMax, title, author, genre)
     where = " price >= " + priceMin + " AND price <= " + priceMax;
 
     if(title != "")
-        where += " AND title = \"" + title + "\"";
+        where += " AND title LIKE \'%" + title + "%\'";
     if(author != "")
-        where += " AND author = \"" + author + "\"";
+        where += " AND author LIKE \'%" + author + "%\'";
     if(genre != "")
-        where += " AND genre = \"" + genre + "\"";
-    
-    //where = " price >= " + priceMin + " AND price <= " + priceMax + " AND author = \"" + author + "\" AND genre = \"" + genre + "\"";
-   // console.log(`SELECT * FROM book WHERE` + where);
+    {
+        if(Array.isArray(genre))
+        {
+            where += " AND (genre = \"" + genre[0] + "\"";
+            for(var i=1; i < genre.length; i++)
+            {
+                where += " OR genre = \"" + genre[i] + "\"";
+            }
+            where += ")";
+        }
+        else
+        {
+            where += " AND genre = \"" + genre + "\"";
+        }
+    }
+
+    console.log(where);
 
     db.serialize(() => {
         db.each(`SELECT * FROM book WHERE` + where, (err, row) => {
@@ -49,5 +55,5 @@ function getBooks(priceMin, priceMax, title, author, genre)
         console.error(err.message);
         }
         console.log('Close the database connection.');
-    });
+    });  
 };
